88579add87586907c2a211b42860037092431b2a
Lokesh9079 <lokeshyogi978@gmail.com>
2023-09-19 08:54:44 +0530

    -

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,41 @@ function scrollFunction() {
 
 
 
-// drop down 
+// channel img owl carousel
+$(document).ready(function() {
+$(".channelCarousel").owlCarousel({
+    margin: 20,
+    nav: false,
+    dots: false,
+    loop:true,
+    autoplay: true,
+    slideTransition: 'linear',
+    autoplayTimeout: 3000,
+    autoplaySpeed: 3000,
+    autoplayHoverPause: false,
+    responsive: {
+      0: {
+        items: 1,
+        dots: true,
+      },
+      576: {
+        items: 1.3,
+        dots: true,
+      },
+      768: {
+        items: 2,
+        dots: true,
+      },
+      992: {
+        items: 2,
+        dots: true,
+      },
+      1200: {
+        items: 2,
+        dots: false,
+      },
+    },
+  });
+  });
 
 
